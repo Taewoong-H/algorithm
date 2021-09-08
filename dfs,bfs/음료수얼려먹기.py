243027bc10b3d97ca2cf933eb6cdd4b64b0e540c
Taewoong-H <taeung2008@gmail.com>
2021-09-08 16:05:36 +0900

    feat: 0908 TIL

--- dfs,bfs/음료수얼려먹기.py
+++ dfs,bfs/음료수얼려먹기.py
@@ -5,14 +5,13 @@
 # N, M 입력
 n, m = map(int, input().split())
 
+
 # 그래프 입력
 graph = []
 for i in range(n):
     graph.append(list(map(int, input())))
 
 # DFS로 특정한 노드 방문한 뒤에 연결된 모든 노드들 방문
-
-
 def dfs(x, y):
     if x <= -1 or x >= n or y <= -1 or y >= m:
         return False
@@ -25,7 +24,6 @@ def dfs(x, y):
         dfs(x, y + 1)
         return True
     return False
-
 
 result = 0
 for i in range(n):
